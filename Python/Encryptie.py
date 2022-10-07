90f3b1cea22d7b41429a24d9085954a15a65a915
AhriVastaya <ahritheionian@gmail.com>
2022-10-07 23:01:52 +0200

    Update Encryptie.py

--- Python/Encryptie.py
+++ Python/Encryptie.py
@@ -6,14 +6,17 @@ from typing import Type
 def GetType(val):
     #Try all possible type and return a number based on the result (1 = int; 2 = float; 3 = string).
     #Also catch all errors to make sure the runtime doesn't get interrupted.
+    #Test if you can cast it to an integer. If not, move on.
     try:
         int(val)
         return 1
     except ValueError: pass
+    #Test if you can cast it to an floating point. If not, move on.
     try:
         float(val)
         return 2
     except ValueError: pass
+    #Test if you can cast it to an string. It's not needed, but I added it for the sake of completion.
     try:
         str(val)
         return 3
@@ -31,6 +34,7 @@ ShiftValue = int(ShiftValue)
 #Initialize Lists for the split list.
 EvenChars = []
 OddChars = []
+
 Index = 0
 #Sorting every element based on it's index.
 for i in InputString:
@@ -54,15 +58,22 @@ for i in EvenChars:
         _ord = ord(i) - 48
         #Shift the ASCII code of the element by the amount defined by the user.
         _ord += ShiftValue
-        #Take the modulo of 10 to make sure that _ord has a value between 0 and 10.
+        #Take the modulo of 10 to make sure that _ord has a value between 0 and 9.
         _ord = _ord % 10
         #Add the 48 back in.
         _ord = _ord + 48
     elif GetType(i) == 3:
+        #ord(0) = 97, so I subtract it by 48 to make the modulus calculation work.
         _ord = ord(i) - 97
+        #Shift the ASCII code of the element by the amount defined by the user.
         _ord += ShiftValue
+        #Take the modulo of 10 to make sure that _ord has a value between 0 and 25.
         _ord = _ord % 26
+        #Add the 97 back in.
         _ord = _ord + 97
+    #Generate the character that corresponds to the ASCII code of _ord and store it in i (for competion, actually unnecessary).
     i = chr(_ord)
+    #Write the shifted character to the list of elements with an even number.
     EvenChars[Index] = i
+    #Increment the index to move on to the next element.
     Index += 1
