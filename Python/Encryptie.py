518bf65fd3a2b292cba6d3d28c51bd57b116757f
AhriVastaya <ahritheionian@gmail.com>
2022-10-09 15:35:00 +0200

    Update Encryptie.py

--- Python/Encryptie.py
+++ Python/Encryptie.py
@@ -52,11 +52,8 @@ for i in InputString:
 #Manipulate the characters from the list containing the elements with even indexes by applying a Caesar-encryption to them.
 Index = 0
 for i in EvenChars:
-    #Leave any spaces alone, and do nothing with that element
-    if i == " ":
-        pass
     #If the element is an integer, there are only 10 possibilities.
-    elif GetType(i) == 1:
+    if GetType(i) == 1:
         #ord(0) = 48, so I subtract it by 48 to make the modulus calculation work.
         _ord = ord(i) - 48
         #Shift the ASCII code of the element by the amount defined by the user.
@@ -66,14 +63,18 @@ for i in EvenChars:
         #Add the 48 back in.
         _ord = _ord + 48
     elif GetType(i) == 3:
-        #ord(0) = 97, so I subtract it by 48 to make the modulus calculation work.
-        _ord = ord(i) - 97
-        #Shift the ASCII code of the element by the amount defined by the user.
-        _ord += ShiftValue
-        #Take the modulo of 10 to make sure that _ord has a value between 0 and 25.
-        _ord = _ord % 26
-        #Add the 97 back in.
-        _ord = _ord + 97
+        #Leave any spaces alone, and do nothing with that element
+        if i == " ":
+            _ord = ord(i)
+        else:
+            #ord(0) = 97, so I subtract it by 48 to make the modulus calculation work.
+            _ord = ord(i) - 97
+            #Shift the ASCII code of the element by the amount defined by the user.
+            _ord += ShiftValue
+            #Take the modulo of 10 to make sure that _ord has a value between 0 and 25.
+            _ord = _ord % 26
+            #Add the 97 back in.
+            _ord = _ord + 97
     #Generate the character that corresponds to the ASCII code of _ord and store it in i (for competion, actually unnecessary).
     i = chr(_ord)
     #Write the shifted character to the list of elements with an even number.
